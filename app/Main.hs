13da2863ba602ee697d05d2e5e8fbff313d052e7
stanislav-az <st.zhdanovich@gmail.com>
2018-12-20 20:22:41 +0700

    warp test

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+app :: Application
+app _ respond = do
+    putStrLn "I've done some IO here"
+    respond $ responseLBS
+        status200
+        [("Content-Type", "text/plain")]
+        "Hello, Web!"
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn $ "http://localhost:8080/"
+    run 8080 app
