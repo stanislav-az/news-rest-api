f445d1ed482143c65c47fc9004587943458052c5
stanislav-az <st.zhdanovich@gmail.com>
2019-01-18 15:02:26 +0700

    route tests

--- src/Router.hs
+++ src/Router.hs
@@ -7,51 +7,39 @@ import           Data.Text
 import           Network.Wai
 import           Network.HTTP.Types
 
--- data List a = Cons a List | Nil
 data Route = PathRoute Text Route | DynamicRoute Text Route | MethodRoute ByteString
-type Handler = Request -> Response
+type Handler = Request -> IO Response
 
--- /api/users/55/assign POST
-userAssignRoute :: Route
-userAssignRoute =
-    PathRoute "api"
-        $ PathRoute "users"
-        $ DynamicRoute "55"
-        $ PathRoute "assign"
-        $ MethodRoute "POST"
-
-userRejectRoute :: Route
-userRejectRoute =
-    PathRoute "api"
-        $ PathRoute "users"
-        $ DynamicRoute "55"
-        $ PathRoute "reject"
-        $ MethodRoute "POST"
-
+createAuthorRoute :: Route
+createAuthorRoute = PathRoute "api" $ PathRoute "author" $ MethodRoute "POST"
 
 routes :: [(Route, Handler)]
 routes =
-    [ ( userAssignRoute
-      , const $ responseLBS status200 [("Content-Type", "text/html")] "Ok"
-      )
-    , ( userRejectRoute
-      , const $ responseLBS status410 [("Content-Type", "text/html")] "NotOk"
-      )
-    ]
+  [ ( createAuthorRoute
+    , const $ pure $ responseLBS status200 [("Content-Type", "text/html")] "Ok"
+    )
+  , ( MethodRoute "GET"
+    , const $ pure $ responseLBS status200 [("Content-Type", "text/html")] "Ok"
+    )
+  ]
 
--- reqToRoute :: Request -> Route
--- reqToRoute req = let path = pathInfo req in
-
-parsePath :: Route -> [Text] -> ByteString ->  Bool
-parsePath (MethodRoute x) [] method | x == method = True
-                                    | otherwise   = False
-parsePath (MethodRoute x) xs method = False
-parsePath route           [] _      = False
-parsePath (PathRoute s route) (x : xs) method
-    | x == s    = parsePath route xs method
-    | otherwise = False
-parsePath (DynamicRoute s route) (x : xs) method = parsePath route xs method
+isCorrectRoute :: Route -> [Text] -> ByteString -> Bool
+isCorrectRoute (MethodRoute x) [] method | x == method = True
+                                         | otherwise   = False
+isCorrectRoute (MethodRoute x) xs method = False
+isCorrectRoute route           [] _      = False
+isCorrectRoute (PathRoute s route) (x : xs) method
+  | x == s    = isCorrectRoute route xs method
+  | otherwise = False
+isCorrectRoute (DynamicRoute s route) (x : xs) method =
+  isCorrectRoute route xs method
 
 
-route :: [(Route, Handler)] -> Request -> Response
-route handlers req = undefined
+route :: [(Route, Handler)] -> Request -> IO Response
+route [] req = pure $ responseLBS status404 [("Content-Type", "text/html")] "Not found"
+route (h:hs) req | isCorrectRoute currentRoute path method = snd h req
+  | otherwise = route hs req
+  where
+    currentRoute = fst h
+    path = pathInfo req
+    method = requestMethod req
--- test/RouteSpec.hs
+++ test/RouteSpec.hs
@@ -6,9 +6,18 @@ module RouteSpec
 where
 
 import           Test.Hspec
-import           Router                         ( parsePath
+import           Router                         ( isCorrectRoute
                                                 , Route(..)
+                                                , route
                                                 )
+import           Network.Wai
+import           Network.HTTP.Types
+import qualified Data.ByteString.Lazy          as L
+import           Data.ByteString.Builder        ( Builder
+                                                , toLazyByteString
+                                                , word8
+                                                )
+import           Data.IORef
 
 rootRoute = MethodRoute "GET"
 
@@ -31,47 +40,105 @@ userRejectRoute =
         $ MethodRoute "POST"
 
 spec :: Spec
-spec = describe "route" $ do
-    it "should route to root url GET" $ do
-        let res = parsePath rootRoute [] "GET"
-        res `shouldBe` True
-    it "should not route to root url with wrong request method" $ do
-        let res = parsePath rootRoute [] "POST"
-        res `shouldBe` False
-    it "should not route to non-existing GET url" $ do
-        let res = parsePath rootRoute ["api", "unknown", "123"] "GET"
-        res `shouldBe` False
-    it "should route to user assign page for user 55 with POST" $ do
-        let
-            res = parsePath userAssignRoute
-                            ["api", "users", "55", "assign"]
-                            "POST"
-        res `shouldBe` True
-    it "should route to user reject page for user 55 with POST" $ do
-        let
-            res = parsePath userRejectRoute
-                            ["api", "users", "55", "reject"]
-                            "POST"
-        res `shouldBe` True
-    it "should not route if url is longer than existing routes" $ do
-        let res = parsePath userRejectRoute
-                            ["api", "users", "55", "reject", "something"]
-                            "POST"
-        res `shouldBe` False
-    it "should not route if url is shorter than existing routes" $ do
-        let res = parsePath userRejectRoute ["api", "users"] "POST"
-        res `shouldBe` False
-    it "should not route if url is empty" $ do
-        let res = parsePath userRejectRoute [] "POST"
-        res `shouldBe` False
-    it "should not route if has wrong sub-path" $ do
-        let res =
-                parsePath userRejectRoute ["api", "user", "55", "reject"] "POST"
-        res `shouldBe` False
-    it "should route to dynamic root route" $ do
-        let res =
-                parsePath dynamicRoute ["123"] "POST"
-        res `shouldBe` True
+spec = do
+    describe "isCorrectRoute" $ do
+        it "should route to root url GET" $ do
+            let res = isCorrectRoute rootRoute [] "GET"
+            res `shouldBe` True
+        it "should not route to root url with wrong request method" $ do
+            let res = isCorrectRoute rootRoute [] "POST"
+            res `shouldBe` False
+        it "should not route to non-existing GET url" $ do
+            let res = isCorrectRoute rootRoute ["api", "unknown", "123"] "GET"
+            res `shouldBe` False
+        it "should route to user assign page for user 55 with POST" $ do
+            let
+                res = isCorrectRoute userAssignRoute
+                                     ["api", "users", "55", "assign"]
+                                     "POST"
+            res `shouldBe` True
+        it "should route to user reject page for user 55 with POST" $ do
+            let
+                res = isCorrectRoute userRejectRoute
+                                     ["api", "users", "55", "reject"]
+                                     "POST"
+            res `shouldBe` True
+        it "should not route if url is longer than existing routes" $ do
+            let res = isCorrectRoute
+                    userRejectRoute
+                    ["api", "users", "55", "reject", "something"]
+                    "POST"
+            res `shouldBe` False
+        it "should not route if url is shorter than existing routes" $ do
+            let res = isCorrectRoute userRejectRoute ["api", "users"] "POST"
+            res `shouldBe` False
+        it "should not route if url is empty" $ do
+            let res = isCorrectRoute userRejectRoute [] "POST"
+            res `shouldBe` False
+        it "should not route if has wrong sub-path" $ do
+            let
+                res = isCorrectRoute userRejectRoute
+                                     ["api", "user", "55", "reject"]
+                                     "POST"
+            res `shouldBe` False
+        it "should route to dynamic root route" $ do
+            let res = isCorrectRoute dynamicRoute ["123"] "POST"
+            res `shouldBe` True
 
+    describe "route" $ do
+        let responseOk =
+                responseLBS status200 [("Content-Type", "text/html")] "Ok"
+            response400 = responseLBS status400
+                                      [("Content-Type", "text/html")]
+                                      "Not valid"
+            response404 = responseLBS status404
+                                      [("Content-Type", "text/html")]
+                                      "Not found"
+            response500 = responseLBS status500
+                                      [("Content-Type", "text/html")]
+                                      "Sorry"
+            createAuthorRoute =
+                PathRoute "api" $ PathRoute "author" $ MethodRoute "POST"
 
+            routes = [(createAuthorRoute, const $ pure responseOk)]
+            getBody res = do
+                let (_, _, f) = responseToStream res
+                f $ \streamingBody -> do
+                    builderRef <- newIORef mempty
+                    let add :: Builder -> IO ()
+                        add b = atomicModifyIORef builderRef
+                            $ \builder -> (builder `mappend` b, ())
+                        flush :: IO ()
+                        flush = return ()
+                    streamingBody add flush
+                    fmap (L.toStrict . toLazyByteString) $ readIORef builderRef
+        it "should not call handler on incorrect request" $ do
+            res <- route routes defaultRequest
+            getBody res `shouldReturn` "Not found"
+            responseStatus res `shouldBe` status404
+        it "should call create author handler on POST /api/author" $ do
+            let authorReq = defaultRequest { requestMethod = "POST"
+                                           , pathInfo      = ["api", "author"]
+                                           }
+            res <- route routes authorReq
+            getBody res `shouldReturn` "Ok"
+            responseStatus res `shouldBe` status200
+        it "should not call create author handler on methods other than POST"
+            $ do
+                  let authorReq = defaultRequest { requestMethod = "GET"
+                                                 , pathInfo = ["api", "author"]
+                                                 }
+                  res <- route routes authorReq
+                  getBody res `shouldReturn` "Not found"
+                  responseStatus res `shouldBe` status404
+        it "should not call create author handler on incorrect path" $ do
+            let authorReq = defaultRequest { requestMethod = "POST"
+                                           , pathInfo      = ["api", "authors"]
+                                           }
+            res <- route routes authorReq
+            getBody res `shouldReturn` "Not found"
+            responseStatus res `shouldBe` status404
+        -- TODO :
+        -- emty routes
+        -- duplicate routes
 
